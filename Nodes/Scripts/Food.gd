8d55b93405680b2c87c8d4cd93f72387cd85886f
Lucas Rufino <lucaslrt@outlook.com>
2019-11-01 17:08:06 -0300

    Init game project.

---
+++ Nodes/Scripts/Food.gd
@@ -0,0 +1,37 @@
+extends RigidBody2D
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+const GRAVITY = 10
+const FLOOR = Vector2(0, -1)
+
+var velocity = Vector2()
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+#	velocity.y += GRAVITY
+#	velocity = move_and_slide(velocity, FLOOR)
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+#func _on_Food_body_entered(body) -> void:
+#	print("Colidiu com %s", body.name)
+#	pass # Replace with function body.
+
+func apply_inertia_velocity(velocity: Vector2):
+	self.velocity = velocity
+	pass
+
+func _on_Area2D_body_entered(body) -> void:
+	if self.name != body.name:
+		if body.name == "Plate":
+			apply_inertia_velocity(body.velocity)
+		elif body.name.match("Food"):
+			body.apply_inertia_velocity(velocity*1.1)
+		print(self.name, " colidiu com ", body.name)
+	pass # Replace with function body.
---
+++ Nodes/Scripts/Player.gd
@@ -0,0 +1,131 @@
+extends KinematicBody2D
+
+export(int) var SPEED = 30
+export(int) var hp = 1
+const GRAVITY = 0
+const JUMP_FORCE = -250
+const FLOOR = Vector2(0, -1)
+
+const FIREBALL = preload("res://Nodes/Prefabs/Fireball.tscn")
+
+var velocity = Vector2(0,0)
+var on_ground = false
+var is_attacking = false
+var is_dead = false
+var fireball_power = 1
+var jump_count = 0
+
+func _ready() -> void:
+	adjust_camera_limits()
+	pass
+
+func adjust_camera_limits() -> void:
+	var tilemap_rect = get_parent().get_node("TileMap").get_used_rect()
+	var tilemap_cell_size = get_parent().get_node("TileMap").cell_size
+	$Camera2D.limit_left = tilemap_rect.position.x * tilemap_cell_size.x
+	$Camera2D.limit_right = tilemap_rect.end.x * tilemap_cell_size.x
+	$Camera2D.limit_top = tilemap_rect.position.y * tilemap_cell_size.y
+	$Camera2D.limit_bottom = tilemap_rect.end.y * tilemap_cell_size.y
+	pass
+
+func _physics_process(delta: float) -> void:
+
+	velocity.x = 0
+	if !is_dead:
+		if Input.is_action_pressed("ui_right"):
+#			if !is_attacking or !is_on_floor():
+			velocity.x += SPEED
+
+#				if !is_attacking:
+#					$AnimatedSprite.flip_h = false
+#					$AnimatedSprite.play("run")
+#					if sign($Plate.position.x) == -1:
+#						$Plate.position.x *= -1
+		if Input.is_action_pressed("ui_left"):
+#			if !is_attacking or !is_on_floor():
+			velocity.x -= SPEED
+
+		if Input.is_action_just_pressed("ui_focus_next"):
+			get_tree().reload_current_scene()
+
+
+#				if !is_attacking:
+#					$AnimatedSprite.flip_h = true
+#					$AnimatedSprite.play("run")
+#					if sign($Plate.position.x) == 1:
+#						$Plate.position.x *= -1
+#		else:
+#			velocity.x = 0
+#			if on_ground == true and !is_attacking:
+#				$AnimatedSprite.play("idle")
+
+#		if Input.is_action_just_pressed("ui_up"):
+##			velocity.y = JUMP_FORCE
+##			on_ground = false
+#			jump_count += 1
+#			print("jump_count: %d" % jump_count)
+#			if jump_count <= 2:
+#				velocity.y = JUMP_FORCE
+#				on_ground = false
+
+#		if Input.is_action_just_pressed("ui_focus_next") and !is_attacking:
+#			if is_on_floor():
+#				velocity.x = 0
+#			is_attacking = true
+#			$AnimatedSprite.play("attack")
+#			var fireball = FIREBALL.instance()
+#			fireball.set_fireball_power(fireball_power)
+#
+#			if sign($Plate.position.x) == 1:
+#				fireball.set_fireball_direction(1)
+#			else:
+#				fireball.set_fireball_direction(-1)
+#			get_parent().add_child(fireball)
+#			fireball.position = $Position2D.global_position
+#
+		velocity.y += GRAVITY * delta
+
+		move_and_slide(velocity, FLOOR)
+
+#		if is_on_floor():
+#			if !on_ground:
+#				on_ground = true
+#				is_attacking = false
+#				jump_count = 0
+#		else:
+#			if !is_attacking:
+#				if on_ground:
+#					on_ground = false
+#					jump_count = 1
+#				if velocity.y < 0:
+#					$AnimatedSprite.play("jump")
+#				else:
+#					$AnimatedSprite.play("fall")
+#
+#		if get_slide_count() > 0:
+#			for i in range(get_slide_count()):
+#				if "Enemy" in get_slide_collision(i).collider.name:
+#					dead()
+
+	pass
+
+func dead():
+	is_dead = true
+	velocity = Vector2(0,0)
+	$AnimatedSprite.play("dead")
+	$CollisionShape2D.set_deferred("disabled", true)
+	$CollisionShape2D2.set_deferred("disabled", true)
+	$Timer.start()
+
+func _on_AnimatedSprite_animation_finished() -> void:
+	is_attacking = false
+	pass # Replace with function body.
+
+
+func _on_Timer_timeout() -> void:
+	get_tree().change_scene("res://Nodes/Scenes/TitleScreen.tscn")
+	pass # Replace with function body.
+
+func crown_power_up() -> void:
+	fireball_power = 2
+	pass
